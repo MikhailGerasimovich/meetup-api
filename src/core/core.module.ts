2263f34240a2951c410fd71bc14ce80a64bd0557
Bear_999_ <mihailgerasimovich2002@gmail.com>
2023-03-02 15:54:03 +0300

    create meetup module

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { MeetupModule } from './meetup/meetup.module';
 
-@Module({})
+@Module({
+  imports: [MeetupModule],
+})
 export class CoreModule {}
