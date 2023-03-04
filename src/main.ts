7584cbe0fb0176881e747e8fb49d7ed71afa54d8
Bear_999_ <mihailgerasimovich2002@gmail.com>
2023-03-04 18:46:16 +0300

    add parameters for validation dto and options

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,13 @@ const host = process.env.HOST ?? 'localhost';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   await app.listen(port, host);
 }
 bootstrap();
