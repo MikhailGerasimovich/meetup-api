b871beb93d31e3d2e3415430eaf30980bb2d9bb2
Bear_999_ <mihailgerasimovich2002@gmail.com>
2023-03-07 00:36:20 +0300

    add tag service

--- src/core/tag/tag.service.ts
+++ src/core/tag/tag.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { CreateTagDto } from './dto/create-tag.dto';
 import { TagOptions } from './dto/tag.options';
@@ -9,13 +9,48 @@ import { Tag } from './tag.model';
 export class TagService {
   constructor(@InjectModel(Tag) private readonly tagRepository: typeof Tag) {}
 
-  public async create(createTagDto: CreateTagDto) {}
+  public async create(createTagDto: CreateTagDto): Promise<Tag> {
+    const existingTag = await this.readOneBy({ name: createTagDto.name });
+    if (existingTag) {
+      throw new BadRequestException(`tag with name=${createTagDto.name} already exists`);
+    }
 
-  public async readAllBy(tagOptions: TagOptions) {}
+    const tag = await this.tagRepository.create(createTagDto);
+    return tag;
+  }
 
-  public async readOneBy(tagOptions: TagOptions) {}
+  public async readAllBy(tagOptions: TagOptions): Promise<Tag[]> {
+    const tags = await this.tagRepository.findAll({ where: { ...tagOptions } });
+    return tags;
+  }
 
-  public async update(id: string, updateTagDto: UpdateTagDto) {}
+  public async readOneBy(tagOptions: TagOptions): Promise<Tag> {
+    const tag = await this.tagRepository.findOne({ where: { ...tagOptions } });
+    return tag;
+  }
 
-  public async delete(id: string) {}
+  public async update(id: string, updateTagDto: UpdateTagDto) {
+    const existingTag = await this.readOneBy({ id });
+    if (existingTag) {
+      throw new NotFoundException(`tag with id=${id} not found`);
+    }
+
+    const sameNameTag = await this.readOneBy({ name: updateTagDto.name });
+    if (sameNameTag) {
+      throw new BadRequestException(`tag with name=${updateTagDto.name} already exists`);
+    }
+
+    await this.tagRepository.update(updateTagDto, { where: { id } });
+
+    const updatedTag = await this.readOneBy({ id });
+    return updatedTag;
+  }
+
+  public async delete(id: string): Promise<void> {
+    const existingTag = await this.readOneBy({ id });
+    if (existingTag) {
+      throw new NotFoundException(`tag with id=${id} not found`);
+    }
+    await this.tagRepository.destroy({ where: { id } });
+  }
 }
