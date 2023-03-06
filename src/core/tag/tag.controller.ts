6c754ec1f285a099e3eff945fa5bfc5fe89dd188
Bear_999_ <mihailgerasimovich2002@gmail.com>
2023-03-07 01:42:13 +0300

    fix tag controller and registration tag service and controller

--- src/core/tag/tag.controller.ts
+++ src/core/tag/tag.controller.ts
@@ -3,6 +3,7 @@ import { HttpStatus } from '@nestjs/common/enums';
 import { CreateTagDto } from './dto/create-tag.dto';
 import { UpdateTagDto } from './dto/update-tag.dto';
 import { TagService } from './tag.service';
+import { Tag } from './tag.model';
 
 @Controller('tag')
 export class TagController {
@@ -10,21 +11,35 @@ export class TagController {
 
   @Post()
   @HttpCode(HttpStatus.CREATED)
-  public async create(@Body() createTagDto: CreateTagDto) {}
+  public async create(@Body() createTagDto: CreateTagDto): Promise<Tag> {
+    const tag = await this.tagService.create(createTagDto);
+    return tag;
+  }
 
   @Get()
   @HttpCode(HttpStatus.OK)
-  public async readAll() {}
+  public async readAll(): Promise<Tag[]> {
+    const tags = await this.tagService.readAllBy({});
+    return tags;
+  }
 
   @Get(':id')
   @HttpCode(HttpStatus.OK)
-  public async readById(@Param('id') id: string) {}
+  public async readById(@Param('id') id: string): Promise<Tag> {
+    const tag = await this.tagService.readOneBy({ id });
+    return tag;
+  }
 
   @Put(':id')
   @HttpCode(HttpStatus.OK)
-  public async update(@Param('id') id: string, @Body() updateTagDto: UpdateTagDto) {}
+  public async update(@Param('id') id: string, @Body() updateTagDto: UpdateTagDto): Promise<Tag> {
+    const updatedTag = await this.tagService.update(id, updateTagDto);
+    return updatedTag;
+  }
 
   @Delete(':id')
-  @HttpCode(HttpStatus.OK)
-  public async delete(@Param('id') id: string) {}
+  @HttpCode(HttpStatus.NO_CONTENT)
+  public async delete(@Param('id') id: string): Promise<void> {
+    await this.tagService.delete(id);
+  }
 }
--- src/core/tag/tag.module.ts
+++ src/core/tag/tag.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { TagController } from './tag.controller';
 import { Tag } from './tag.model';
+import { TagService } from './tag.service';
 
 @Module({
   imports: [SequelizeModule.forFeature([Tag])],
+  controllers: [TagController],
+  providers: [TagService],
 })
 export class TagModule {}
