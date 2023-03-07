f91acc6a8e5a745dd8810a7233e1489900efddaf
Bear_999_ <mihailgerasimovich2002@gmail.com>
2023-03-08 00:35:01 +0300

    export and import tags service

--- src/core/meetup/meetup.module.ts
+++ src/core/meetup/meetup.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { TagModule } from '../tag/tag.module';
 import { MeetupController } from './meetup.controller';
 import { Meetup } from './meetup.model';
 import { MeetupService } from './meetup.service';
 
 @Module({
-  imports: [SequelizeModule.forFeature([Meetup])],
+  imports: [SequelizeModule.forFeature([Meetup]), TagModule],
   controllers: [MeetupController],
   providers: [MeetupService],
 })
