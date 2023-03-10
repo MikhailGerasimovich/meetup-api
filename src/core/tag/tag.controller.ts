3fb41f5e1bcdb33bf671ce21c9ac27c7e9f38cd7
Bear_999_ <mihailgerasimovich2002@gmail.com>
2023-03-10 23:47:47 +0300

    add types for tag's controller methods

--- src/core/tag/tag.controller.ts
+++ src/core/tag/tag.controller.ts
@@ -3,7 +3,7 @@ import { HttpStatus } from '@nestjs/common/enums';
 import { CreateTagDto } from './dto/create-tag.dto';
 import { UpdateTagDto } from './dto/update-tag.dto';
 import { TagService } from './tag.service';
-import { Tag } from './tag.model';
+import { FrontendTag } from './types/tag.types';
 
 @Controller('tag')
 export class TagController {
@@ -11,30 +11,33 @@ export class TagController {
 
   @Post()
   @HttpCode(HttpStatus.CREATED)
-  public async create(@Body() createTagDto: CreateTagDto): Promise<Tag> {
+  public async create(@Body() createTagDto: CreateTagDto): Promise<FrontendTag> {
     const tag = await this.tagService.create(createTagDto);
-    return tag;
+    return new FrontendTag(tag);
   }
 
   @Get()
   @HttpCode(HttpStatus.OK)
-  public async readAll(): Promise<Tag[]> {
+  public async readAll(): Promise<FrontendTag[]> {
     const tags = await this.tagService.readAllBy({});
-    return tags;
+    return tags.map((tag) => new FrontendTag(tag));
   }
 
   @Get(':id')
   @HttpCode(HttpStatus.OK)
-  public async readById(@Param('id') id: string): Promise<Tag> {
+  public async readById(@Param('id') id: string): Promise<FrontendTag> {
     const tag = await this.tagService.readOneBy({ id });
-    return tag;
+    return new FrontendTag(tag);
   }
 
   @Put(':id')
   @HttpCode(HttpStatus.OK)
-  public async update(@Param('id') id: string, @Body() updateTagDto: UpdateTagDto): Promise<Tag> {
+  public async update(
+    @Param('id') id: string,
+    @Body() updateTagDto: UpdateTagDto,
+  ): Promise<FrontendTag> {
     const updatedTag = await this.tagService.update(id, updateTagDto);
-    return updatedTag;
+    return new FrontendTag(updatedTag);
   }
 
   @Delete(':id')
