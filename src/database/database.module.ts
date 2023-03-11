ac8f786fa88dc66721f292421919dd9810c1d7d6
Mikhail <97798185+MikhailGerasimovich@users.noreply.github.com>
2023-03-11 15:43:39 +0300

    Revert "Extension meetup api"

--- src/database/database.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SequelizeModule } from '@nestjs/sequelize';
-import { dbconfig } from 'src/configs/db.config';
-
-@Module({
-  imports: [SequelizeModule.forRoot(dbconfig)],
-})
-export class DatabaseModule {}
