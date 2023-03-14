f89ac2081dceb08f435e8695078b85213a0c1eca
Bear_999_ <mihailgerasimovich2002@gmail.com>
2023-03-14 14:34:41 +0300

    fix conflicts with dev branch

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { AppModule } from './app.module';
+import { config } from 'dotenv';
+config();
+
+const port = Number(process.env.PORT) ?? 3000;
+const host = process.env.HOST ?? 'localhost';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  await app.listen(port, host);
+}
+bootstrap();
