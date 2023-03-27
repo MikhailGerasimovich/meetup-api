a073672a13fc25cb5b72d9800e7eb2de1565b410
Bear_999_ <mihailgerasimovich2002@gmail.com>
2023-03-28 01:26:24 +0300

    add jwt and local strategy

---
+++ src/core/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common/decorators';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PayloadDto } from '../dto/payload.dto';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_ACCESS_TOKEN_SECRET,
+    });
+  }
+
+  public async validate(payload: PayloadDto) {
+    return payload;
+  }
+}
