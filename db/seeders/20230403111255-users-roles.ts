c04fb6125f2c4e1ec8ecec41ee2079a105454bac
Bear_999_ <mihailgerasimovich2002@gmail.com>
2023-06-05 13:27:46 +0300

    change seeds

--- db/seeders/20230403111255-users-roles.ts
+++ db/seeders/20230403111255-users-roles.ts
@@ -6,14 +6,10 @@ module.exports = {
       {
         user_id: '461fbe94-b182-4f89-9de0-baaf95fc0684',
         role_id: '1d40f052-8231-4414-ad10-389655761315',
-        createdAt: new Date(),
-        updatedAt: new Date(),
       },
       {
         user_id: '1d40f052-8231-4414-ad10-389655761315',
         role_id: 'aa52124e-bbf5-7324-be3c-9cf0bf8f67f1',
-        createdAt: new Date(),
-        updatedAt: new Date(),
       },
     ]);
   },
